a49a169bff33068dae20ca0fd07669b3912a240f
sivameii <sivameilindaa@gmail.com>
2024-09-26 23:34:08 +0700

    Add files via upload

---
+++ Siva 1.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() 
+{
+    char nim[] = "2441086"; 
+    char nama[] = "Siva Meilinda"; 
+    char jurusan[] = "Teknik Informatika";
+
+    printf("NIM: %s\n", nim);
+    printf("NAMA: %s\n", nama);
+    printf("JURUSAN: %s\n", jurusan);
+
+    return 0;
+}
+
---
+++ siva 2.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    float panjang = 10.0; 
+    float lebar = 7.0;   
+    float luas;
+
+    luas = panjang * lebar;
+
+    printf("Luas Persegi Panjang: %.2f cm^2\n", luas);
+
+    return 0;
+}
+
